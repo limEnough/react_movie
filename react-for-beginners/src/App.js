3f774bf8e57504131679eb0169b19cceb9f48f49
limenough <limenough@github.com>
2023-09-17 23:43:21 +0900

    feat: useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,12 +1,24 @@
 
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+
+  const onClick = () => setValue((prev) => prev + 1);
+
+  console.log('i run all the time.');
+
+  useEffect(() => {
+    console.log('CALL THE API...');
+  }, []);
+  
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"continue"}/>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      {/* <Button text={"continue"}/> */}
     </div>
   );
 }
