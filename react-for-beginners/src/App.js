5d3fccc52f9b257a7d1ad5bbf688f857afe325be
limenough <limenough@github.com>
2023-11-05 23:08:14 +0900

    feat: To Do List part1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,29 +1,18 @@
 
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function destroyedFn() {
-    console.log("bye :(");
-  }
-  function effectFn () {
-    // hello 컴포넌트를 hide할 때에는 컴포넌트 자체를 그리지 않기에 콘솔로그도 찍히지 않음.
-    console.log("created :)");
-
-    // clean-up function : 컴포넌트가 사라질 때 실행되는 함수는 return 문에 작성
-    return destroyedFn;
-  }
-  useEffect(effectFn, []);
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const onchange = (event) => setToDo(event.target.value);
+  console.log(toDo);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form>
+        <input onChange={onchange} value={toDo} type="text" placeholder="write your to do.." />
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
